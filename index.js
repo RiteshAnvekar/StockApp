9b7150f5a634ddb4c4160c0a49a959ea67e21293
Ritesh A <riteshanvekar98@gmail.com>
2023-01-30 19:03:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 5000
+
+// Middleware for Handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine','handlebars');
+
+
+//Set handlebar routes
+app.get('/', function(req,res){
+	res.render('home');
+})
+
+//Set static folder
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.listen(PORT, () => console.log("Server listening on PORT " + PORT));
